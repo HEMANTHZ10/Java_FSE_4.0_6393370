572cd8670164ad8b4ef8e481221edfe062a2431f
HEMANTHZ10 <peralahemanth10@gmail.com>
2025-07-16 17:56:45 +0530

    WEEK_4 IS COMPLETED

---
+++ src/W4_Spring_Rest_Handson/controller/CountryController.java
@@ -0,0 +1,33 @@
+package Week4_spring_rest_handson.controller;
+
+import com.cognizant.springlearn.Country;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+//
+@RestController
+public class CountryController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    @GetMapping("/country")
+    public Country getCountryIndia() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = context.getBean("country", Country.class);
+        return country;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        List<Country> list = (List<Country>) context.getBean("countryList");
+        return list.stream()
+                .filter(c -> c.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null);
+    }
+}
