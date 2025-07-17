4505f30799561ff8bf1db51fe97934f286aed7e6
HEMANTHZ10 <peralahemanth10@gmail.com>
2025-07-17 18:09:33 +0530

    WEEK_6 IS COMPLETED

---
+++ src/W5_Microservices/account/cognizant_Account/AccountApplication.java
@@ -0,0 +1,9 @@
+package Week5_Microservices.account.cognizant_Account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
